fd713605f37679d8520898f680fad541f720beac
titus <contact@christitus.com>
2026-04-12 13:01:04 -0500

    fix: correct directory path in rebuild-css script

--- scripts/rebuild-css.sh
+++ scripts/rebuild-css.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/www/discourse/plugins/discourse-community-plugins
+cd /var/www/discourse/plugins/discourse-community-integrations
 git pull
 su discourse -c "cd /var/www/discourse && RAILS_ENV=production bundle exec rake tmp:clear assets:clean assets:precompile"
 sv restart unicorn
